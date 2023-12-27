ea32f0f2f7e43fc1aa0ad2a2f586ec360189023d
vikash singh <vikashsingh77162@gmail.com>
2023-12-27 11:20:35 +0530

    first commit

---
+++ comparison_of_datatype.js
@@ -0,0 +1,33 @@
+// we can compare between to operand by many comparision operater 
+/*
+
+for example 2>1
+here 2 and 1 are operand andcharacter "> " it is comparison operater 
+when it is true than return true else false
+55<66
+55==55
+
+*/
+console.log(2>1);
+console.log(55<66)
+console.log(55==55);
+
+
+
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined<=0);
+
+// console.log("2"==2);
+// console.log("3">2);
+// avoid comparison between deffrent datatype 
+
+
+console.log("2"===2);
+
