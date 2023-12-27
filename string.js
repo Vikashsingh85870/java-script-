a35aea223b3eef5a70c245a13e2a8c706f161e05
vikash singh <vikashsingh77162@gmail.com>
2023-12-27 12:52:46 +0530

    first

---
+++ string.js
@@ -0,0 +1,22 @@
+// string can be denoted as single and   double quete ' ," "
+/*
+
+*/
+const name ="vikash"
+const number = 50
+console.log(name+number);
+console.log(number +name);
+console.log("vikash"+"kumar"+6);
+// but in this way the code is not reliable so we use another way
+console.log(`hello my name is ${name }`);
+
+console.log(name[0]);
+// prototype of string or method
+console.log(name.length)
+console.log(name.toUpperCase())
+console.log(name.charAt(2));
+console.log(name.indexOf('h'))
+
+
+const str= name.substring(0,4);
+console.log(str);
