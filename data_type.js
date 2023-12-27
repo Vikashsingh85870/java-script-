e40a171bb3e9cfdd9c5c3892889f394af75fb688
vikash singh <vikashsingh77162@gmail.com>
2023-12-27 11:58:43 +0530

    data tye

---
+++ data_type.js
@@ -0,0 +1,41 @@
+/*
+data two are categaries in two form based on how to store in it in memory and how to  access it 
+onee is primmitive 
+non primmitive
+
+*/
+// ******************************************************//
+/*
+primitive
+string
+number,bigINt
+boolean
+NULL
+undefined
+symbol 
+
+
+
+*/
+const quantity=100
+const quality= "good"
+const flag =true
+
+const a= symbol('123');
+const b= symbol('123');
+console.log(a===b);
+
+//********************************************************* */
+/*
+Non primitive
+Arrsy
+object
+function
+
+*/
+const heros=["vikash","rohit","subham"]
+let myobj = {
+    name:"vikash",
+    age:22
+
+}
